c8ec3553bf4bf6086ef26b471dbc69c5f7e18710
IzumiFushina <125682908+IzumiFushina@users.noreply.github.com>
2024-02-21 12:13:03 -0300

    Lição 21/02 I2HN expo

--- App.js
+++ App.js
@@ -4,7 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={[styles.texto, {color:'blue'}]}><b>Gustavo Gabriel Cunha Manhães</b></Text>
+      <Text style={[styles.notebook, {color:'black'}]}><b>19</b></Text>      
+      <Text style={[styles.sala, {color:'red'}]}><b>Turma: I2HN</b></Text>     
+      <Text style={[styles.sala, {color:'red'}]}><b>Sala: 7</b></Text>     
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightblue',
     alignItems: 'center',
     justifyContent: 'center',
+    width: '100%',
+    weight: '100%',
   },
-});
+  texto: {
+    fontSize: 30,
+    left: '30%', 
+  },
+  notebook: {
+    fontSize: 25,
+    fontStyle: 'italic',
+    right: '30%',
+    borderBottomColor: 'black',
+    borderBottomWidth: 2,
+    top: '25%',
+  },
+  sala: {
+    backgroundColor: 'pink',
+    fontSize: 20,
+  }
+  },
+
+);
